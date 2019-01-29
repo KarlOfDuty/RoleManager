25c4e2612b0f41359d95a68c5866ff09bbdef5d6
Karl Essinger <xkaess22@gmail.com>
2019-01-30 00:54:24 +0100

    Added yaml npm package

---
+++ node_modules/yaml/dist/polyfill.js
@@ -0,0 +1,27 @@
+"use strict";
+
+require("core-js/modules/es6.array.sort");
+
+require("core-js/modules/es7.object.define-getter");
+
+require("core-js/modules/es7.object.define-setter");
+
+require("core-js/modules/es7.object.entries");
+
+require("core-js/modules/es7.object.get-own-property-descriptors");
+
+require("core-js/modules/es7.object.lookup-getter");
+
+require("core-js/modules/es7.object.lookup-setter");
+
+require("core-js/modules/es7.object.values");
+
+require("core-js/modules/es7.promise.finally");
+
+require("core-js/modules/es7.symbol.async-iterator");
+
+require("core-js/modules/es7.string.pad-start");
+
+require("core-js/modules/es7.string.pad-end");
+
+require("./index");
